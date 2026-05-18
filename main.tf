91a70cd4829749fa0b3c64c60f5291fbd767ed85
root <root@DESKTOP-SNMGH55.localdomain>
2026-05-18 19:31:52 +0200

    initial terraform S3 bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "monitoring_logs" {
+  bucket = "monitoring-logs-nexeruno"
+}
